4959a712d45e3905d738e13f1d7c0b9a8dfa2cd2
Nicolai Berthelsen <nicolai@bibsyst.no>
2018-06-29 11:17:04 +0200

    remove unused dependency import

--- src/components/book.js
+++ src/components/book.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 // Material UI
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import Chip from "@material-ui/core/Chip";
 
 // Redux
 import { connect } from "react-redux";
