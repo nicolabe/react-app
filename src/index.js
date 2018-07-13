a5d6ef4a8ebd919dc7d51907a60eaef42ffff826
Nicolai Berthelsen <nicolai@bibsyst.no>
2018-07-13 09:57:29 +0200

    we don't use service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
 // Redux
 import { Provider } from "react-redux";
@@ -13,5 +12,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-registerServiceWorker();
